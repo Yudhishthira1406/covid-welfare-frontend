4d81fa46d3b66552a1991a6da7334580d95f6193
unknown <ddaksh.2711@gmail.com>
2020-10-23 19:57:01 +0530

    Add Login with Google in home page and redirect to MyProfile Page

---
+++ src/MyProfile.js
@@ -0,0 +1,15 @@
+import React,{ Component } from 'react';
+
+class MyProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() {
+        return (
+                <h1>My Profile</h1>
+            
+        )}
+}
+
+export default MyProfile;
