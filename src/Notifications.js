78d846ecda825ebec2179114e83b40ed8d2abec0
shivam8812 <shivag4321@gmail.com>
2020-11-23 00:32:15 +0530

    updtaed notification styling

--- src/Notifications.js
+++ src/Notifications.js
@@ -15,7 +15,7 @@ class Notifications extends Component {
     constructor(props){
         super(props);
         this.state = {
-            provideReqs:[],seekReqs:[],othersReqs:[],clickedUser:[]
+            provideReqs:[],seekReqs:[],othersReqs:[],clickedUser:[],seektext:""
         }
         this.getProvideReqs=this.getProvideReqs.bind(this);
         this.getSeekReqs=this.getSeekReqs.bind(this);
@@ -99,6 +99,19 @@ class Notifications extends Component {
         .catch(error => {
             console.log(error);
         })
+        axios.get(`http://127.0.0.1:8000/api/${username}/seek`,{
+            headers: {
+                'Authorization': `Token ${localStorage.getItem('token')}`
+            }
+        })      
+        .then(response => {
+            this.setState({
+                seektext:response.data.seek_text,
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
     render(){
         return(
@@ -110,36 +123,49 @@ class Notifications extends Component {
                     <div className="notification-center">
                         <h4>Your provide requests</h4>
                         <div>
+                            <ul>
                             {this.state.provideReqs.map(request => (
+                                <li>
                                 <div>
-                                    <p>You have requested to help</p> <p value={request} className="req-name" onClick={() => this.showDetails(request)}> {request}</p>
-                                </div>                            
+                                    <text>You have requested to help</text> <text value={request} className="req-name" onClick={() => this.showDetails(request)}> {request}</text>
+                                </div>  
+                                </li>                          
                             ))}
+                            </ul>
                         </div>
                         <h4>Your seek requests</h4>
                         <div>
+                            <ul>
                             {this.state.seekReqs.map(request => (
+                                <li>
                                 <div>
                                     <p>You have requested to get help from</p> <p value={request} className="req-name" onClick={() => this.showDetails(request)}> {request}</p>
                                 </div>
+                                </li>
                             ))}
+                            </ul>
                         </div>
                         <h4>People who have requested from you</h4>
                         <div>
+                            <ul>
                             {this.state.othersReqs.map(request => (
+                                <li>
                                 <div>
-                                    <p value={request} className="req-name" onClick={() => this.showDetails(request.text.split(" ")[0])}>{request.text}</p><p>{request.time}</p><br />
+                                    <p value={request} className="req-name" onClick={() => this.showDetails(request.text.split(" ")[0])}>{request.text}</p><p>{request.time}</p>
                                 </div>
+                                </li>
                             ))}
+                            </ul>
                         </div>
                     </div>
                     <div className="notification-right">
-                        <div>
-                            <h4>Adress:<br /> {this.state.clickedUser.address}</h4>
-                            <h4>blood group:<br /> {this.state.clickedUser.blood_group}</h4>
-                            <h4>contact:<br /> {this.state.clickedUser.contact}</h4>
-                            <h4>occupation:<br /> {this.state.clickedUser.occupation}</h4>
-                            <h4>username:<br /> {this.state.clickedUser.username}</h4>
+                        <div className="details">
+                            <div className="detail-heading">Username</div><div className="detail-text"> {this.state.clickedUser.username}</div>
+                            <div className="detail-heading">Adrress</div><div className="detail-text"> {this.state.clickedUser.address}</div>
+                            <div className="detail-heading">Blood group</div><div className="detail-text"> {this.state.clickedUser.blood_group}</div>
+                            <div className="detail-heading">Contact number</div><div className="detail-text"> {this.state.clickedUser.contact}</div>
+                            <div className="detail-heading">Occupation</div><div className="detail-text"> {this.state.clickedUser.occupation}</div>
+                            <div className="detail-heading">Seektext</div><div className="detail-text"> {this.state.seektext}</div>
                         </div>
                     </div>
                 </div>
